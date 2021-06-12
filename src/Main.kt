4cc53fb8603a0fec1ff3836e42f8b8c325904364
m7mdra <xm7mdrax@gmail.com>
2021-06-12 22:34:38 +0200

    more options

--- src/Main.kt
+++ src/Main.kt
@@ -10,6 +10,14 @@ fun main(args: Array<String>) {
 
         input("file.mp4")
         output("file.mvi")
+        videoOption(Resize(110 x 300))
+        videoOption(ResizeToPredefined(FrameSize.qqvga))
+        videoOption(FrameRate(10))
+        videoOption(BitRate(10))
+        FileSize(10 * 1024 * 1024)
+
+        RelativeScale(RelativeDimension("iw/2", "ih/2"))
+        Scale(200 x 200)
     }
     val build = ffmpeg.build()
     print(build)
@@ -19,6 +27,7 @@ fun main(args: Array<String>) {
 class FFMPEGBuilder {
     private var input: String? = null
     private var output: String? = null
+    private val options = mutableListOf<Option<*>>()
     fun input(path: String) {
         this.input = path
     }
@@ -36,9 +45,10 @@ class FFMPEGBuilder {
 
     }
 
-    fun inputOption(key: String, value: String) {
-
+    fun videoOption(option: Option<*>) {
+        options.add(option)
     }
+
 
     fun build(): String {
 
@@ -47,34 +57,35 @@ class FFMPEGBuilder {
 
 }
 
-class FrameRate(rate: Int) : VideoOption<Int>(key = "-r", rate) {}
-class BitRate(rate: Int) : VideoOption<Int>(key = "-b:v", rate) {}
-class FileSize(rate: Int) : VideoOption<Int>(key = "-b:v", rate) {}
-class Resize : VideoOption<Int> {
-    constructor(size: FrameSize) : super(key = "-s", size.ordinal)
-    constructor(size: Int) : super(key = "-s", size)
-}
-
-abstract class ScaleOption<T>(val width: T, val height: T)
-
-class RelativeScaleOption() : ScaleOption<String>("iw", "ih") {
+class FrameRate(rate: Int) : Option<Int>(key = "-r", rate) {}
+class BitRate(rate: Int) : Option<Int>(key = "-b:v", rate) {}
+class FileSize(size: Int) : Option<Int>(key = "-fs", size) {}
+class ResizeToPredefined(size: FrameSize) : Option<FrameSize>(key = "-s", size)
+class Resize(size: Dimension) : Option<Dimension>(key = "-s", size)
 
 
-}
+class Scale(dimension: Dimension) : Option<Dimension>("-s", dimension)
+class RelativeScale(relativeDimension: RelativeDimension) :
+    Option<RelativeDimension>("-vf scale=", relativeDimension)
 
+enum class FrameSize(size: Dimension) {
 
-class Scale : VideoOption<Any>("-vf scale=") {}
-
-enum class FrameSize(size: Int) {
-    sqcif(128 * 96),
-    qqvga(160 * 120)
+    sqcif(128 x 96),
+    qqvga(160 x 120)
     //TODO list other frameSizes
 }
 
-abstract class VideoOption<T>(val key: String, val t: T)
+abstract class Option<T>(val key: String, val t: T)
 
 fun ffmpeg(builder: FFMPEGBuilder.() -> Unit): FFMPEGBuilder {
     val ffmpegBuilder = FFMPEGBuilder()
     ffmpegBuilder.builder()
     return ffmpegBuilder
+}
+
+class Dimension(val width: Int, val height: Int)
+class RelativeDimension(val widthWithModifier: String, val heightWithModifier: String)
+
+infix fun Int.x(height: Int): Dimension {
+    return Dimension(this, height)
 }
